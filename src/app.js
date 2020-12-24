520503e8e2bb7b6288121134983f5081fe65f567
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2020-12-24 00:13:19 -0500

    trying to solve cors issue

--- src/app.js
+++ src/app.js
@@ -13,8 +13,14 @@ const app = express();
 
 const morganOption = (NODE_ENV === 'production')
 
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
 app.use(cors());
 app.options('*', cors()); 
+app.use(allowCrossDomain)
 app.use(morgan(morganOption));
 app.use(helmet());
 
@@ -30,11 +36,6 @@ app.get("/", (req, res) => {
 });
 
 module.exports = app;
-
-const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
   // intercept OPTIONS method
   if ('OPTIONS' == req.method) {
